a4512ed62c4a3626b9a1a7da70107f0f452ce9c1
zbyju <zbynek.jurica@gmail.com>
2022-11-07 00:01:45 +0200

    Improve tests

--- src/__tests__/BinarySearchList.ts
+++ src/__tests__/BinarySearchList.ts
@@ -1,13 +1,22 @@
-import binary_fn from "@code/BinarySearchList"
+import binary_fn from "@code/BinarySearchList";
 
-test("binary search array", function() {
+function testAll(array: number[]) {
+    array.forEach((x) => expect(binary_fn(array, x)).toEqual(true));
+}
 
+test("binary search array - odd length", function () {
     const foo = [1, 3, 4, 69, 71, 81, 90, 99, 420, 1337, 69420];
-    expect(binary_fn(foo, 69)).toEqual(true);
+    testAll(foo);
     expect(binary_fn(foo, 1336)).toEqual(false);
-    expect(binary_fn(foo, 69420)).toEqual(true);
     expect(binary_fn(foo, 69421)).toEqual(false);
-    expect(binary_fn(foo, 1)).toEqual(true);
     expect(binary_fn(foo, 0)).toEqual(false);
 });
 
+test("binary search array - even length", function () {
+    const foo = [1, 3, 4, 69, 71, 81, 90, 99, 420, 1337, 69420, 6942069];
+    testAll(foo);
+    expect(binary_fn(foo, 1336)).toEqual(false);
+    expect(binary_fn(foo, 69421)).toEqual(false);
+    expect(binary_fn(foo, 0)).toEqual(false);
+    expect(binary_fn(foo, 6942070)).toEqual(false);
+});
