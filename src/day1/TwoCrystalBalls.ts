bb8d5ea1f39c3b4f0ccde3d4036eb30fac4b56fd
zbyju <zbynek.jurica@gmail.com>
2022-11-07 22:30:02 +0200

    Implement two crystal balls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,14 @@
+// Breaks [false, false, false, ..., false, true, true, true, ..., true]
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const jmp = Math.floor(Math.sqrt(breaks.length));
+    let i = jmp;
+    while (i < breaks.length && breaks[i] === false) {
+        i += jmp;
+    }
 
+    for (let j = i - jmp; j <= i; ++j) {
+        if (breaks[j] === true) return j;
+    }
+
+    return -1;
 }
